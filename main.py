e28b3f78bca41f6a19f63dac9f4349a845b7dde3
Arkit Sen <senarkit@gmail.com>
2023-08-14 23:54:18 +0530

    added PCA, XGB
    new feature selection procedures added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime
 import yaml
 import pandas as pd
 from Features.selection import feature_selection
+import os
 
 #### import config file
 with open("config.yaml") as f:
@@ -10,7 +11,7 @@ with open("config.yaml") as f:
 
 #### LOGGING
 now = datetime.datetime.now().strftime("%d-%m-%Y-%HH:%MM:%SS")
-logging.basicConfig(filename=f"./logs/LOG_{now}.log", 
+logging.basicConfig(filename= os.path.join(config['path']['logpath'], f"LOG_{now}.log"), 
                     filemode='w', 
                     level = logging.DEBUG,
                     format='[%(asctime)s] : %(name)s - %(funcName)s | %(levelname)s | %(message)s',
@@ -29,7 +30,8 @@ fs = feature_selection(X, y, config['fs']['features'], config['fs']['model'])
 feat_coeff = fs.fit_predict()
 feat_coeff['coeff_abs'] = abs(feat_coeff['Coeff'])
 feat_coeff = feat_coeff.sort_values(by=['coeff_abs'], ascending=[False])
-print(feat_coeff)
+feat_coeff.drop(columns=['coeff_abs'], inplace=True)
+logger.info("Best Features - \n %s",feat_coeff.head(5))
 
 #### END
 logger.debug("END main() script execution")
