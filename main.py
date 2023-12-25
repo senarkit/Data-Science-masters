eaae637afafa9f585b6d78502e0553d0e6336711
Arkit Sen <senarkit@gmail.com>
2023-12-25 19:21:17 +0530

    restructure-tested

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ with open("config.yaml") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
 #### LOGGING
-now = datetime.datetime.now().strftime("%d-%m-%Y-%HH:%MM:%SS")
-logging.basicConfig(filename= os.path.join(config['path']['logpath'], f"LOG_{now}.log"), 
+now = datetime.datetime.now().strftime("%d%m%Y_%HH%MM%SS")
+logging.basicConfig(filename= f"./logs/log_{now}.log", 
                     filemode='w', 
                     level = logging.DEBUG,
                     format='[%(asctime)s] : %(name)s - %(funcName)s | %(levelname)s | %(message)s',
@@ -20,18 +20,22 @@ logger = logging.getLogger(__name__)
 logger.debug('START main() script execution')
 
 
-#### main script code/function calls
-data = pd.read_csv(config['path']['filepath'])
-y = data[config['fs']['target']].copy()
-X = data[config['fs']['features']]
+########### main script code/function calls
 
 ## Feature Selection
-fs = feature_selection(X, y, config['fs']['features'], config['fs']['model'])
-feat_coeff = fs.fit_predict()
-feat_coeff['coeff_abs'] = abs(feat_coeff['Coeff'])
-feat_coeff = feat_coeff.sort_values(by=['coeff_abs'], ascending=[False])
-feat_coeff.drop(columns=['coeff_abs'], inplace=True)
-logger.info("Best Features - \n %s",feat_coeff.head(5))
+if 'fs' in (config['pipeline']):
 
-#### END
+    data = pd.read_csv(config['path']['filepath'])
+    y = data[config['fs']['target']].copy()
+    X = data[config['fs']['features']]
+
+    ## Feature Selection
+    fs = feature_selection(X, y, config['fs']['features'], config['fs']['model'])
+    feat_coeff = fs.fit_predict()
+    feat_coeff['coeff_abs'] = abs(feat_coeff['Coeff'])
+    feat_coeff = feat_coeff.sort_values(by=['coeff_abs'], ascending=[False])
+    feat_coeff.drop(columns=['coeff_abs'], inplace=True)
+    logger.info("Best Features - \n %s",feat_coeff.head(5))
+
+############## END
 logger.debug("END main() script execution")
